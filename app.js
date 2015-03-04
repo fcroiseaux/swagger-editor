ebff209946f8b0ad9b00c761c78d0f404cf2aa95
Fabrice CROISEAUX <fcroiseaux@gmail.com>
2015-03-04 07:59:43 +0100

    Added swagger-ui

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ var bodyparser = require('body-parser');
 var fs = require('fs');
 
 
-app.use('/', express.static('.tmp'));
-app.use('/', express.static('app'));
+app.use('/', express.static('swagger-ui'));
+
+app.use('/editor/', express.static('.tmp'));
+app.use('/editor/', express.static('app'));
 app.use(bodyparser.text());
 
 app.get('/backend', function (req, res) {
-  res.sendFile(__dirname + '/app/swagger.yaml')
+  res.sendFile(__dirname + '/app/postsafe.yaml')
 })
 
 app.put('/backend', function (req, res) {
   //console.log(util.inspect(req.body));
-  fs.writeFile(__dirname + '/app/swagger.yaml', req.body, function(err) {
+  fs.writeFile(__dirname + '/app/postsafe.yaml', req.body, function(err) {
     if(err) {
       console.log(err);
-    } else {
-      console.log("The file was saved!");
     }
   });
   res.send('OK')
@@ -27,22 +27,20 @@ app.put('/backend', function (req, res) {
 
 app.put('/jsonbackend', function (req, res) {
   //console.log(util.inspect(req.body));
-  fs.writeFile(__dirname + '/app/swagger.json', req.body, function(err) {
+  fs.writeFile(__dirname + '/app/postsafe.json', req.body, function(err) {
     if(err) {
       console.log(err);
-    } else {
-      console.log("The file was saved!");
     }
   });
   res.send('OK')
 })
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('PostSafe API documentation manager listening at http://%s:%s', host, port)
 
 })
